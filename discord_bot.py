055652dec828035d2f288a9deb21e0111267aedd
Bebibio <95770149+Bebibio@users.noreply.github.com>
2023-07-29 18:44:03 +0200

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -22,28 +22,4 @@ async def on_message(message):
 bot_token = os.environ.get("DISCORD_BOT_TOKEN")
 
 # Pour lancer le bot
-client.ru n(bot_token) os
-import discord
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-        
-# On récupère notre token discord dans l'env de Railway
-bot_token = os.environ.get("DISCORD_BOT_TOKEN")
-
-# Pour lancer le bot
 client.run(bot_token)
